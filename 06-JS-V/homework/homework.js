7fbb3b4f18a731d8e7d4362e7138fefef3d8a792
Yaileen Manjarres <yaileenmanjarres@gmail.com>
2022-04-05 17:55:35 -0500

    EJERCICIOS JS05

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -85,7 +85,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  const persona = new Persona(nombre, apellido, edad, dir);
+  var persona = new Persona(nombre, apellido, edad, dir);
   return persona;
 }
   
